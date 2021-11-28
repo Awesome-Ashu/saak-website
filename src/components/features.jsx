2e3a44b463127971852c6c3dc417477ffacb43c6
monop0ly <agnihotri_vaibhav@ymail.com>
2021-11-28 14:35:17 +0530

    Update features.jsx

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -8,12 +8,14 @@ export const Features = (props) => {
         <div className='row'>
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className='col-xs-6 col-md-3'>
+                <div key={`${d.title}-${i}`} className='col-md-3 col-sm-6 team'>
+				<div className='thumbnail'>
                   {' '}
 				   <img src={d.img} alt='...' className='features-img' />
                   <h3>{d.title}</h3>
                   <p>{d.text}</p>
                 </div>
+				</div>
               ))
             : 'Loading...'}
         </div>
